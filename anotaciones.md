1b55fc8c1c6a26df412ebc41bbd83f1dbc00c002
Nicolas Rocchio <nicolasrocchio92@gmail.com>
2024-08-06 13:03:14 -0300

    concluyo la etapa 2 del integrador

---
+++ anotaciones.md
@@ -0,0 +1,22 @@
+## Pasos para arracar el proyecto
+
+```sh
+npm create vite@latest ./ -- --template vanilla
+``` 
+
+## Instalar las dependencias (SASS)
+
+```sh
+npm i sass -D
+```
+
+## Creamos el archivo vite.config.js
+
+
+## Motores de plantilla
+
+<https://handlebarsjs.com/installation/#npm-or-yarn-recommended>
+
+```sh
+npm i handlebars
+``` 
---
+++ main.js
@@ -0,0 +1,39 @@
+import './sass/main.scss';
+import Handlebars from 'handlebars';
+
+const start = async () => {
+  try {
+    /* 1. Obtención de la plantilla */
+    const respuesta = await fetch('templates/card.hbs');
+
+    if (!respuesta.ok) {
+      throw new Error('No se pudo obtener la plantilla');
+    }
+
+    const plantilla = await respuesta.text();
+
+    const template = Handlebars.compile(plantilla);
+
+    const respuestaBack = await fetch(
+      'https://66b238061ca8ad33d4f6fda8.mockapi.io/juegos',
+    );
+
+    if (!respuestaBack.ok) {
+      throw new Error('Algo paso con los productos', respuestaBack.status);
+    }
+
+    const dataProductos = await respuestaBack.json();
+    const data = { productos: dataProductos };
+    const html = template(data);
+
+    console.log(html);
+
+    const contenedorCards = document.querySelector('#contenedor-cards');
+
+    contenedorCards.innerHTML = html;
+  } catch (error) {
+    console.log('[start]:', error);
+  }
+};
+
+window.addEventListener('DOMContentLoaded', start);
